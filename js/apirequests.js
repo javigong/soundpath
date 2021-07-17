396aa48658f7542dda8d2375543f0d350cd49522
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-07-17 10:23:00 -0700

    add save playlist function

--- js/apirequests.js
+++ js/apirequests.js
@@ -136,3 +136,49 @@ async function getRecommendedSongs() {
 // getRecommendedSongs();
 
 // end GET Playlist Recommendation  ================================ //
+
+// Async Function to GET User ID, Playlist Name and Save Playlist == //
+
+// Parameters
+let playlistName = "My New Playlist Test";
+
+function savePlaylist(playlistName, uriArr) {
+  if (!playlistName || !uriArr.length) {
+      return;
+  }
+  const accessToken = getAccessToken();
+  const headers = { Authorization: `Bearer ${accessToken}` };
+  let userId;
+
+  return fetch(`https://api.spotify.com/v1/me`, {
+      headers: headers,
+  })
+      .then((response) => {
+          return response.json();
+      })
+      .then((jsonResponse) => {
+          userId = jsonResponse.id;
+          return fetch(
+              `https://api.spotify.com/v1/users/${userId}/playlists`,
+              {
+                  headers: headers,
+                  method: 'POST',
+                  body: JSON.stringify({ name: playlistName }),
+              }
+          )
+              .then((response) => {
+                  return response.json();
+              })
+              .then((jsonResponse) => {
+                  const playlistId = jsonResponse.id;
+                  return fetch(
+                      `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
+                      {
+                          headers: headers,
+                          method: 'POST',
+                          body: JSON.stringify({ uris: uriArr }),
+                      }
+                  );
+              });
+      });
+}
