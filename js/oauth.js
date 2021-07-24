3bfe798d9e2aec1f690b176196388f15e5859650
Javier Gongora Marruedo <42308135+javigong@users.noreply.github.com>
2021-07-24 15:05:04 -0700

    Update oauth.js

--- js/oauth.js
+++ js/oauth.js
@@ -1,7 +1,11 @@
-const redirect_uri = "http://localhost:5501/"; //Replace with you localhost
+const redirect_uri = "https://soundpath.netlify.app/:5501/"; //Replace with you localhost
 
-let client_id = "a0875c5745d349afa70d58abc2368d8b";
-let client_secret = "f3be40b89030473bac2a1cf2cc7cdda2";
+//gustavo
+// let client_id = "a0875c5745d349afa70d58abc2368d8b";
+// let client_secret = "f3be40b89030473bac2a1cf2cc7cdda2";
+//javierg
+let client_id = "ea6dbfb6ca0e451e8fa3da6cfc97b5c7";
+let client_secret = "82e9c8e180cd447aa61bcab97df41a2b";
 
 let access_token = null;
 let refresh_token = null;
@@ -61,8 +65,8 @@ localStorage.setItem("client_id", client_id);
 localStorage.setItem("client_secret", client_secret);
 
 function requestAuthorization() {
-    client_id = "a0875c5745d349afa70d58abc2368d8b";
-    client_secret = "f3be40b89030473bac2a1cf2cc7cdda2";
+    // client_id = "a0875c5745d349afa70d58abc2368d8b";
+    // client_secret = "f3be40b89030473bac2a1cf2cc7cdda2";
 
     let url = AUTHORIZE;
     url += "?client_id=" + client_id;
