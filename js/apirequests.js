fbb5481b584c980db37061370e76081637a40df7
Gustavo <gmonte@tutanota.com>
2021-07-28 22:08:30 -0300

    Update
    Update

--- js/apirequests.js
+++ js/apirequests.js
@@ -2,6 +2,11 @@
 //                   GET User's ID                                    //
 // ================================================================== //
 
+
+const playlistName = document.querySelector('#namePlaylist');
+
+let spotID;
+
 async function getSpotId() {
     let res = await fetch("https://api.spotify.com/v1/me", {
         headers: {
@@ -12,41 +17,45 @@ async function getSpotId() {
     })
 
     let data = await res.json();
-    //user ID
-    let spotID = await data.id;
+    //filter for getting the user ID
+    spotID = await data.id
     console.log(spotID);
+}
+let dummyText = true;
 
-    // ================================================================== //
-    //                  Create Playlist                                   //
-    // ================================================================== //
-
-    /* get playlist's name */
-    const playlistName = document.querySelector('#namePlaylist');
-
-
-     // ISSUE HERE vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-// Problems to pass variables to the body of the Create Playlist API
-
-     fetch("https://api.spotify.com/v1/users/`${spotID}`/playlists", {
-         body: "{"
-         name\\ ":\\"
-         `${playlistName.value}`\\ ",\\"
-         description\\ ":\\"
-         New playlist description\\ ",\\"
-         public\\ ":false}",
-         headers: {
-             Accept: "application/json",
-             Authorization: `Bearer ${access_token}`,
-             "Content-Type": "application/json"
-         },
-         method: "POST"
-     })
-
-
-     // ISSUE HERE ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+function assessCondition() {
+    if (access_token && dummyText) {
+        getSpotId();
+        dummyText = false;
+    }
 }
 
-// getSpotId();
+
+// ================================================================== //
+//                  Create Playlist                                   //
+// ================================================================== //
+
+/* get playlist's name */
+
+// Problems to pass variables to the body of the Create Playlist API
+
+async function createPlaylist() {
+    let playCreation = await fetch(`https://api.spotify.com/v1/users/${spotID}/playlists`, {
+        body: `{"name":"${playlistName.value}","description":"New playlist description","public":false}
+        `,
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${access_token}`,
+            "Content-Type": "application/json",
+        },
+        method: "POST"
+    });
+
+    let anwser = await playCreation.json();
+    console.log(anwser);
+}
+
+
 
 // ================================================================== //
 //                   GET Playlist Recommendation                      //
@@ -196,18 +205,18 @@ function savePlaylist(playlistName, uriArr) {
     let userId;
 
     return fetch(`https://api.spotify.com/v1/me`, {
-            headers: headers,
-        })
+        headers: headers,
+    })
         .then((response) => {
             return response.json();
         })
         .then((jsonResponse) => {
             userId = jsonResponse.id;
             return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-                    headers: headers,
-                    method: "POST",
-                    body: JSON.stringify({ name: playlistName }),
-                })
+                headers: headers,
+                method: "POST",
+                body: JSON.stringify({ name: playlistName }),
+            })
                 .then((response) => {
                     return response.json();
                 })
@@ -215,10 +224,10 @@ function savePlaylist(playlistName, uriArr) {
                     const playlistId = jsonResponse.id;
                     return fetch(
                         `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
-                            headers: headers,
-                            method: "POST",
-                            body: JSON.stringify({ uris: uriArr }),
-                        }
+                        headers: headers,
+                        method: "POST",
+                        body: JSON.stringify({ uris: uriArr }),
+                    }
                     );
                 });
         });
